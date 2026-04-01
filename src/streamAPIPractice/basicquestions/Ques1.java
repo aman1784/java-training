f7cb883572154cbd00a0ee1c45bf9da5c9dfe3f5
AMAN KUMAR SETH <amanthecoder04@gmail.com>
2026-04-01 23:23:12 +0530

    Problem Statement: Get a list of customer names who have placed orders with amount greater than 1000.

---
+++ src/streamAPIPractice/basicquestions/Ques1.java
@@ -0,0 +1,33 @@
+package streamAPIPractice.basicquestions;
+
+import streamAPIPractice.Order;
+
+import java.time.LocalDate;
+import java.util.List;
+
+// Problem Statement:
+// Get a list of customer names who have placed orders with amount greater than 1000.
+
+// Expected Concepts:
+// -> filter
+// -> map
+public class Ques1 {
+    static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order(1, "Aman", 1200, "DELIVERED", LocalDate.now().minusDays(2), List.of("Laptop", "Mouse")),
+                new Order(2, "Rahul", 800, "PLACED", LocalDate.now().minusDays(1), List.of("Keyboard")),
+                new Order(3, "Aman", 1500, "SHIPPED", LocalDate.now().minusDays(3), List.of("Monitor")),
+                new Order(4, "Sneha", 2000, "DELIVERED", LocalDate.now().minusDays(5), List.of("Phone", "Charger")),
+                new Order(5, "Rahul", 500, "CANCELLED", LocalDate.now().minusDays(4), List.of("Cable"))
+        );
+
+        double threshold = 1000;
+
+        List<String> customerNames = orders.stream()
+                .filter(order -> order.getAmount() > threshold)
+                .map(Order::getCustomerName)
+                .toList();
+
+        System.out.println(customerNames);
+    }
+}
