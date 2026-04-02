f48255f630da374a37e6635b3c2bc54fbb004155
AMAN KUMAR SETH <amanthecoder04@gmail.com>
2026-04-03 01:34:06 +0530

    // Problem: // Get top 3 highest amount orders // // Expected Concepts: // -> sorted() // -> limit()

---
+++ src/streamAPIPractice/beginner/Ques16.java
@@ -0,0 +1,33 @@
+package streamAPIPractice.beginner;
+
+import streamAPIPractice.Order;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+
+// Problem:
+// Get top 3 highest amount orders
+//
+// Expected Concepts:
+// -> sorted()
+// -> limit()
+public class Ques16 {
+
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order(1, "Aman", 1200, "DELIVERED", LocalDate.now().minusDays(2), List.of("Laptop", "Mouse")),
+                new Order(2, "Rahul", 800, "PLACED", LocalDate.now().minusDays(1), List.of("Keyboard")),
+                new Order(3, "Aman", 1500, "SHIPPED", LocalDate.now().minusDays(3), List.of("Monitor")),
+                new Order(4, "Sneha", 2000, "DELIVERED", LocalDate.now().minusDays(5), List.of("Phone", "Charger")),
+                new Order(5, "Rahul", 500, "CANCELLED", LocalDate.now().minusDays(4), List.of("Cable"))
+        );
+
+        List<Order> threeHighestAmountOrdersDesc = orders.stream()
+                .sorted(Comparator.comparingDouble(Order::getAmount).reversed())
+                .limit(3)
+                .toList();
+
+        threeHighestAmountOrdersDesc.forEach(System.out::println);
+    }
+}
