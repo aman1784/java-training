82223d73bbfe7df198410642a71bda0b66d1ac93
AMAN KUMAR SETH <amanthecoder04@gmail.com>
2026-04-07 23:17:15 +0530

    // Problem: // Find Most Ordered Item: Flatten all items from all orders into a single list: Extract Unique Items // // Expected Concepts: // -> flatMap()

---
+++ src/streamAPIPractice/intermediate/Ques9.java
@@ -0,0 +1,35 @@
+package streamAPIPractice.intermediate;
+
+import streamAPIPractice.Order;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// Problem:
+// Find Most Ordered Item: Flatten all items from all orders into a single list: Extract Unique Items
+//
+// Expected Concepts:
+// -> flatMap()
+public class Ques9 {
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order(1, "Aman", 1200, "DELIVERED", LocalDate.now().minusDays(2), List.of("Laptop", "Mouse")),
+                new Order(2, "Rahul", 800, "PLACED", LocalDate.now().minusDays(1), List.of("Keyboard")),
+                new Order(3, "Aman", 1500, "SHIPPED", LocalDate.now().minusDays(3), List.of("Monitor")),
+                new Order(4, "Sneha", 2000, "DELIVERED", LocalDate.now().minusDays(5), List.of("Phone", "Charger")),
+                new Order(5, "Rahul", 500, "CANCELLED", LocalDate.now().minusDays(4), List.of("Cable", "Charger"))
+        );
+
+        Map<String, Long> itemCount = orders.stream()
+                .flatMap(order -> order.getItems().stream())
+                .collect(Collectors.groupingBy(
+                        item -> item,
+                        Collectors.counting()
+                ));
+
+        System.out.println(itemCount);
+
+    }
+}
